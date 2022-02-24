81293d40435f8d37fbd98bacaa4316d49044e75f
elfernando2418 <limefeja8995543@estudiantes3.edu.ec>
2022-02-24 00:57:06 -0500

    xd

--- core/modules/ventas/view/lote_view/widget-default.php
+++ core/modules/ventas/view/lote_view/widget-default.php
@@ -5,48 +5,49 @@
 ?>
  
 
-<table>
-    <thead>
-        <th>HOLA</th>
-        <th>HOLA</th>
-		<th>HOLA</th>
-		<th>HOLA</th>
-		<th>acciones</th>
-    </thead>
-    <tbody>
-	 
-        <?php
-		foreach($lotes as $lote  ){?>
 
-        <tr>
-            <td>  <?php echo  $lote->name;    ?></td>
-			<td>  <?php echo  $lote->num_lot;    ?></td>
-			<td>  <?php echo  $lote->dimension;    ?></td>
-			<td>  <?php echo  $lote->condicion;    ?></td>
-			<td> 
-				<a href="index.php?view=editLote_view&amp;id=<?php echo $lote->id ?>">EDITAR</a>
-			</td>
-        </tr>
+<a type="button" href="index.php?view=addLote_view" class="btn btn-primary">Agregar producto</a>
+<br><br>
+        <!-- DataTales Example -->
+		<div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="95%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Position</th>
+                                            <th>Office</th>
+                                            <th>Age</th>
+                                           
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+																		
+										<?php
+										foreach($lotes as $lote  ){?>
 
-        <?php
-		}
+										<tr>
+											<td>  <?php echo  $lote->name;    ?></td>
+											<td>  <?php echo  $lote->num_lot;    ?></td>
+											<td>  <?php echo  $lote->dimension;    ?></td>
+											<td>  <?php echo  $lote->condicion;    ?></td>
+											<td> 
+												<a href="index.php?view=editLote_view&amp;id=<?php echo $lote->id ?>">EDITAR</a>
+											</td>
+										</tr>
 
-	?>
+										<?php
+										}
 
-
-
-
-
-    </tbody>
-</table>
-
-
-
-<br><br><br><br>
-
-<a href="index.php?view=addLote_view">ME VOY A OTRA PESTANIA</a>
-
+									?>
 
 
-
-<br><br><br><br><br><br>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
